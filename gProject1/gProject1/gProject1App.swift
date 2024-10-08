09ca693579496ead8b2f5139917b8e9713b275b7
somin <somin@sominui-MacBookPro.local>
2024-10-08 22:14:51 +0900

    swiftUI

---
+++ gProject1/gProject1/gProject1App.swift
@@ -0,0 +1,17 @@
+//
+//  gProject1App.swift
+//  gProject1
+//
+//  Created by somin on 10/6/24.
+//
+
+import SwiftUI
+
+@main // 앱의 진입점
+struct gProject1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView() // 앱이 시작될 때 첫 화면으로 ContentView를 표시
+        }
+    }
+}
