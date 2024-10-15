9a22103b3215060778816745ccde6071252ffab9
somin <somin@sominui-MacBookPro.local>
2024-10-15 12:12:39 +0900

    consent,info,main

---
+++ first/Main/Main/MainApp.swift
@@ -0,0 +1,17 @@
+//
+//  MainApp.swift
+//  Main
+//
+//  Created by somin on 10/15/24.
+//
+
+import SwiftUI
+
+@main
+struct MainApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
