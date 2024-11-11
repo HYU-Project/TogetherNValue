501f9c44f04e373d09a56a296be5a40a5a145f2a
Kyoungjihn <jihn@example.com>
2024-11-12 01:14:13 +0900

    MyHome.swift file replaced

--- first/Home/Home/MyHome.swift
+++ first/Home/Home/MyHome.swift
@@ -1,152 +1,87 @@
 //
 //  MyHome.swift
-//  Home
+//  frontproject
 //
-//  Created by 김현경 on 10/30/24.
+//  Created by ace on 10/15/24.
 //
+
 import SwiftUI
 
 struct MyHome: View {
     var body: some View {
-        NavigationView {
-            VStack {
-                VStack(spacing: 16) {
-                    Image(systemName: "person.circle")
-                        .resizable()
-                        .frame(width: 100, height: 100)
-                        .clipShape(Circle())
-                    
-                    Text("김무명")
-                        .font(.title)
-                        .bold()
-                    
-                    Text("한양대학교 서울캠")
-                        .font(.subheadline)
-                    
-                    Text("컴퓨터소프트웨어학과")
-                        .font(.subheadline)
-                    
-                    Button(action: {
-                    }) {
-                        Text("프로필 수정")
-                            .font(.callout)
-                            .padding(.horizontal, 20)
-                            .padding(.vertical, 8)
-                            .background(Color.gray.opacity(0.2))
-                            .cornerRadius(10)
-                    }
-                }
-                .padding(.top, 30)
-                
-                Divider()
-                
+            VStack(alignment: .leading) {
                 HStack {
+                    Text("마이홈")
+                        .font(.title)
+                }
+                HStack{
+                    VStack{
+                        me.profileImage
+                            .resizable()
+                            .clipShape(.circle)
+                        NavigationLink{ProfileEditor()}label: {
+                            Text("프로필 수정")
+                                .foregroundStyle(.black)
+                                .border(.black)
+                        }
+                    }
+                    VStack{
+                        Text(me.name)
+                        Text(me.univCamp)
+                        Text(me.department)
+                    }
+                }
+                Divider()
+                HStack{
                     Text("매너 온도")
-                        .font(.headline)
-                    Spacer()
-                    Text("40.5 도")
-                        .font(.headline)
+                    Text(String(me.temperature)+"도")
                 }
-                .padding()
-                
                 Divider()
-                
-                VStack(alignment: .leading, spacing: 20) {
-                    Text("나의 거래")
-                        .font(.headline)
-                        .padding(.leading)
-                    
-                    NavigationLink{StarPosts()}label:{
-                        HStack(){
-                            Image(systemName:"heart")
-                            Text("관심 목록")
-                            Spacer()
-                        }
-                        .padding(.horizontal)
+                Text("나의 거래")
+                    .font(.title2)
+                NavigationLink{StarPosts()}label:{
+                    HStack(){
+                        Image("Heart")
+                            .resizable()
+                            .frame(width: 50, height: 50)
+                        Text("관심 목록")
                     }
-                    .foregroundStyle(.black)
-                    
-                    NavigationLink{MyPosts()}label:{
-                        HStack(){
-                            Image(systemName:"pencil")
-                            Text("내가 작성한 게시글")
-                            Spacer()
-                        }
-                        .padding(.horizontal)
-                    }
-                    .foregroundStyle(.black)
-                    
-                    NavigationLink{ParticipatePosts()}label:{
-                        HStack(){
-                            Image(systemName:"hand.raised")
-                            Text("참여한 거래")
-                            Spacer()
-                        }
-                        .padding(.horizontal)
-                    }
-                    .foregroundStyle(.black)
-    
                 }
-                .padding(.top)
-                
+                .foregroundStyle(.black)
+                NavigationLink{MyPosts()}label:{
+                    HStack{
+                        Image("post")
+                            .resizable()
+                            .frame(width: 50, height: 50)
+                        Text("내가 작성한 게시글")
+                    }
+                }
+                .foregroundStyle(.black)
+                NavigationLink{ParticipatePosts()}label:{
+                    HStack{
+                        Image("Handshake")
+                            .resizable()
+                            .frame(width: 50, height: 50)
+                        Text("참여한 거래")
+                    }
+                }
+                .foregroundStyle(.black)
                 Divider()
-
-                VStack(alignment: .leading, spacing: 15) {
+                HStack{
                     Text("기타")
-                        .font(.headline)
-                        .padding(.leading)
-
-                    NavigationLink(destination: PolicyView()) {
-                        HStack {
-                            Text("이용약관 (개인 정보 처리 방침)")
-                            Spacer()
-                        }
-                        .padding(.horizontal)
-                    }
-                    
-                    NavigationLink(destination: FAQListView()){
-                        HStack {
-                            Text("자주하는 질문 FAQ")
-                            Spacer()
-                        }
-                        .padding(.horizontal)
-                    }
-                    
-                    
-                    
-                    NavigationLink(destination: AccountInfoView()) {
-                        HStack {
-                            Text("계정 정보")
-                            Spacer()
-                            Text("카카오 (가입일: 2024.9.7)")
-                                .font(.footnote)
-                                .foregroundColor(.gray)
-                        }
-                        .padding(.horizontal)
-                    }
+                        .font(.title2)
                 }
-                .padding(.top)
-                
-                Spacer()
-                Spacer()
-                Spacer()
-                Spacer()
-                Spacer()
-                Spacer()
-                Spacer()
-                Spacer()
-                Spacer()
-                Spacer()
-                Spacer()
-                Spacer()
-                
+                Text("이용약관(개인 정보 처리 방침)\n자주하는 질문 FAQ")
+                HStack {
+                    Text("계정 정보")
+                    Spacer()
+                    Text("카카오 (가입일: 2024.9.7)")
+                }
             }
-            .navigationTitle("마이홈")
-        }
+            .padding()
     }
 }
 
 #Preview {
     MyHome()
 }
-
